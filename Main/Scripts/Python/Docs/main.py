f9819bb67ff4da4484cc233f314e74c919ec5b03
Jack Spencer <UniformKitten281@gmail.com>
2022-06-03 14:54:27 -0700

    Update main.py

--- Main/Scripts/Python/Docs/main.py
+++ Main/Scripts/Python/Docs/main.py
@@ -18,7 +18,25 @@ def main():
     user_input = input("Please enter a number: ")
 
     if user_input == "1":
-        print("")
+        print("README")
+        print("Beans is not Markdown!")
+        print("Create Markdown files easily with Beans, with easy keywords to memorize.")
+        print("The creation of Markdown files will never be hard again!")
+        print("\n")
+        print("Example: (Beans code)")
+        print("\n")
+        print("beans init (Start the beans program)")
+        print("beans create (Create a new file)")
+        print("header I love beans (Create a header with the text I love beans)")
+        print("print Beans are so cool (Add a line of text underneath the header)")
+        print("console I love beans! (Log a line of text to the console)")
+        print("\n")
+        print("The output of the above code will look like this:")
+        print("\n")
+        print("I love beans")
+        print("Beans are so cool")
+        print("\n")
+        main()
 
     elif user_input == "2":
         print("")
