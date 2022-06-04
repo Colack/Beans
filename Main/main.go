c3b98de5e0062597094f7b795d1b5f37983c6085
Jack Spencer <UniformKitten281@gmail.com>
2022-06-03 19:44:44 -0700

    Update main.go

--- Main/main.go
+++ Main/main.go
@@ -69,14 +69,37 @@ func compile(filePath string) {
 
 }
 
-func errorLog(errorMessage string) {
-	// Create a new file, and write to it.
-	f, err := os.Create("error.txt")
-	if err != nil {
-		fmt.Println(err)
+func error(errorMessage string) {
+	typer := os.Args[1:]
+	stringo := os.Args[2:]
+	name := os.Args[3:]
+	if len(typer) != 0 {
+		if typer[0] == "0" {
+			fmt.Println("Creating a file called " + name[0] + "...")
+			f, err := os.Create(name[0])
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("File created.")
+			defer f.Close()
+			fmt.Println("Writing to file...")
+			f.WriteString(stringo[0])
+			fmt.Println("File written.")
+
+		} else if typer[0] == "1" {
+			fmt.Println("Creating a file called " + name[0] + "...")
+			f, err := os.Create(name[0])
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("File created.")
+			defer f.Close()
+			fmt.Println("Writing to file...")
+			f.WriteString(ErrorLog + "\n" + errorMessage + "\n" + ErrorLog)
+			fmt.Println("File written.")
+		} else {
+			fmt.Println("Incorrect usage.")
+			fmt.Println("Correct inputs: \n0 for writing self \n1 for writing default error log")
+		}
 	}
-
-	defer f.Close()
-
-	f.WriteString(ErrorLog + "\n" + errorMessage + "\n" + ErrorLog)
 }
