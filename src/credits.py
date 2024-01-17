96004cd8bd82e90ad26118a36fde4e83197c4d69
Jack Spencer <UniformKitten281@gmail.com>
2024-01-17 09:42:46 -0800

    Create credits.py

---
+++ src/credits.py
@@ -0,0 +1,18 @@
+# Beans Credits
+
+import sys
+import os 
+import time
+
+# Put your name here in the manager variable.
+manager = "Colack"
+
+def main():
+    print("Credits: ")
+    print("Concept by Colack")
+    print("\n")
+    print("Contributors: ")
+    print("Colack")
+    print("Managed by " + manager + ".")
+    x = input("Press anything to continue.")
+main()
